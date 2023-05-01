a75782f03b408ec926fdf9a7b09101b59c7a6199
berkmeric <berkmeric@sabanciuniv.edu>
2023-05-01 22:04:33 +0300

    TicTacToe-BerkM

--- Client/Client/Form1.Designer.cs
+++ Client/Client/Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Client
+﻿namespace client
 {
     partial class Form1
     {
@@ -29,13 +28,139 @@ namespace Client
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.logs = new System.Windows.Forms.RichTextBox();
+            this.textBox_message = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 458);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 496);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port:";
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(293, 455);
+            this.textBox_ip.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(116, 22);
+            this.textBox_ip.TabIndex = 2;
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(293, 493);
+            this.textBox_port.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(116, 22);
+            this.textBox_port.TabIndex = 3;
+            this.textBox_port.TextChanged += new System.EventHandler(this.textBox_port_TextChanged);
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(316, 542);
+            this.button_connect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(93, 27);
+            this.button_connect.TabIndex = 4;
+            this.button_connect.Text = "connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // logs
+            // 
+            this.logs.Location = new System.Drawing.Point(578, 22);
+            this.logs.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.logs.Name = "logs";
+            this.logs.Size = new System.Drawing.Size(372, 596);
+            this.logs.TabIndex = 5;
+            this.logs.Text = "";
+            this.logs.TextChanged += new System.EventHandler(this.logs_TextChanged);
+            // 
+            // textBox_message
+            // 
+            this.textBox_message.Enabled = false;
+            this.textBox_message.Location = new System.Drawing.Point(15, 490);
+            this.textBox_message.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox_message.Name = "textBox_message";
+            this.textBox_message.Size = new System.Drawing.Size(129, 22);
+            this.textBox_message.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 458);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Message:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button_send
+            // 
+            this.button_send.Enabled = false;
+            this.button_send.Location = new System.Drawing.Point(15, 526);
+            this.button_send.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(87, 32);
+            this.button_send.TabIndex = 8;
+            this.button_send.Text = "send";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(962, 629);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_message);
+            this.Controls.Add(this.logs);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.textBox_ip);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.RichTextBox logs;
+        private System.Windows.Forms.TextBox textBox_message;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_send;
     }
 }
 
--- Client/Client/Form1.cs
+++ Client/Client/Form1.cs
@@ -4,17 +4,197 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Client
+namespace client
 {
     public partial class Form1 : Form
     {
+
+        bool terminating = false;
+        bool connected = false;
+        Button[,] gameBoardButtons = new Button[3, 3];
+        Socket clientSocket;
+
         public Form1()
         {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
             InitializeComponent();
+            InitializeGameBoard();
+        }
+
+        private void InitializeGameBoard()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    gameBoardButtons[i, j] = new Button();
+                    gameBoardButtons[i, j].Size = new Size(100, 100);
+                    gameBoardButtons[i, j].Location = new Point(100 * i, 100 * j);
+                    gameBoardButtons[i, j].Click += new EventHandler(GameBoardButton_Click);
+                    gameBoardButtons[i, j].Enabled = false;
+                    this.Controls.Add(gameBoardButtons[i, j]);
+                }
+            }
+        }
+
+
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string IP = textBox_ip.Text;
+
+            int portNum;
+            if(Int32.TryParse(textBox_port.Text, out portNum))
+            {
+                try
+                {
+                    clientSocket.Connect(IP, portNum);
+                    button_connect.Enabled = false;
+                    textBox_message.Enabled = true;
+                    button_send.Enabled = true;
+                    connected = true;
+                    logs.AppendText("Connected to the server!\n");
+
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            gameBoardButtons[i, j].Enabled = true;
+                        }
+                    }
+
+
+                    Thread receiveThread = new Thread(Receive);
+                    receiveThread.Start();
+
+                }
+                catch
+                {
+                    logs.AppendText("Could not connect to the server!\n");
+                }
+            }
+            else
+            {
+                logs.AppendText("Check the port\n");
+            }
+
+        }
+
+        private void Receive()
+        {
+            while (connected)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64];
+                    clientSocket.Receive(buffer);
+
+                    string incomingMessage = Encoding.Default.GetString(buffer);
+                    incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
+
+                    if (incomingMessage == "invalid move")
+                    {
+
+
+                        logs.AppendText("Invalid move: The selected square has already been selected.\n");
+                    }
+                    else
+                    {
+
+                        
+
+                        logs.AppendText("Server: " + incomingMessage + "\n");
+                    }
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        logs.AppendText("The server has disconnected\n");
+                        button_connect.Enabled = true;
+                        for (int i = 0; i < 3; i++)
+                        {
+                            for (int j = 0; j < 3; j++)
+                            {
+                                gameBoardButtons[i, j].Enabled = false;
+                            }
+                        }
+                    }
+
+                    clientSocket.Close();
+                    connected = false;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            connected = false;
+            terminating = true;
+            Environment.Exit(0);
+        }
+
+        private void button_send_Click(object sender, EventArgs e)
+        {
+            string message = textBox_message.Text;
+
+            if(message != "" && message.Length <= 64)
+            {
+                Byte[] buffer = Encoding.Default.GetBytes(message);
+                clientSocket.Send(buffer);
+            }
+
+        }
+
+        private void GameBoardButton_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            int x = clickedButton.Location.X / 100;
+            int y = clickedButton.Location.Y / 100;
+
+            logs.AppendText("Client: " + x + ", " + y + "\n");
+            
+
+            if (connected)
+            {
+                string message = $"move {x} {y}";
+                Byte[] buffer = Encoding.Default.GetBytes(message);
+                clientSocket.Send(buffer);
+            }
+        }
+
+        private void textBox_port_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void logs_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
